1ef447494b084064acf1b08cc0f5831e65bf0501
Amar Alić <amar.alic@edu.fit.ba>
2024-08-05 15:56:36 +0200

    SkolaAdded

---
+++ eDnevnik/eDnevnik.Services/Database/Skola.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eDnevnik.Services.Database
+{
+    public partial class Skola
+    {
+        public int SkolaID { get; set; }
+        public string Naziv { get; set; }
+        public string Adresa { get; set; }
+        public string Grad { get; set; }
+        public string Drzava { get; set; }
+        public virtual ICollection<Odjeljenje> Odjeljenja { get; set; } = new List<Odjeljenje>();
+        public virtual ICollection<Korisnik> Korisnici { get; set; } = new List<Korisnik>();
+        public virtual ICollection<Predmet> Predmeti { get; set; } = new List<Predmet>();
+    }
+}
