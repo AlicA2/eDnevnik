c13f68e502851112739175560c64bbb714a3e1ea
Amar Alić <amar.alic@edu.fit.ba>
2024-08-31 12:14:35 +0200

    DropdownImplement

--- eDnevnik/UI/ednevnik_admin/lib/screens/report_screen.dart
+++ eDnevnik/UI/ednevnik_admin/lib/screens/report_screen.dart
@@ -1,5 +1,13 @@
+import 'package:ednevnik_admin/models/department.dart';
+import 'package:ednevnik_admin/models/school.dart';
+import 'package:ednevnik_admin/models/subject.dart';
+import 'package:ednevnik_admin/providers/department_provider.dart';
+import 'package:ednevnik_admin/providers/department_subject_provider.dart';
+import 'package:ednevnik_admin/providers/school_provider.dart';
+import 'package:ednevnik_admin/providers/subject_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:ednevnik_admin/widgets/master_screen.dart';
+import 'package:provider/provider.dart';
 
 class ReportDetailScreen extends StatefulWidget {
   const ReportDetailScreen({Key? key}) : super(key: key);
@@ -9,6 +17,62 @@ class ReportDetailScreen extends StatefulWidget {
 }
 
 class _ReportDetailScreenState extends State<ReportDetailScreen> {
+  late DepartmentProvider _departmentProvider;
+  late SubjectProvider _subjectProvider;
+  late SchoolProvider _schoolProvider;
+
+  Department? _selectedDepartment;
+  Subject? _selectedSubject;
+  School? _selectedSchool;
+
+  List<Department> _departments = [];
+  List<Subject> _subjects = [];
+  List<School> _schools = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _departmentProvider = context.read<DepartmentProvider>();
+    _subjectProvider = context.read<SubjectProvider>();
+    _schoolProvider = context.read<SchoolProvider>();
+
+    _fetchSchools();
+  }
+
+  Future<void> _fetchSchools() async {
+    try {
+      var schools = await _schoolProvider.get();
+      if (mounted) {
+        setState(() {
+          _schools = schools.result;
+          if (_schools.isNotEmpty) {
+            _selectedSchool = _schools.first;
+            _fetchDepartments();
+            _fetchSubjects();
+          }
+        });
+      }
+    } catch (e) {}
+  }
+
+  Future<void> _fetchDepartments() async {
+    var data = await _departmentProvider.get(filter: {
+      'SkolaID': _selectedSchool?.skolaID,
+    });
+    setState(() {
+      _departments = data.result;
+    });
+  }
+
+  Future<void> _fetchSubjects() async {
+    var data = await _subjectProvider.get(filter: {
+      'SkolaID': _selectedSchool?.skolaID,
+    });
+    setState(() {
+      _subjects = data.result;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MasterScreenWidget(
@@ -24,6 +88,8 @@ class _ReportDetailScreenState extends State<ReportDetailScreen> {
             child: Column(
               children: [
                 _buildScreenName(),
+                SizedBox(height: 16.0),
+                _buildSearch(),
               ],
             ),
           ),
@@ -54,6 +120,80 @@ class _ReportDetailScreenState extends State<ReportDetailScreen> {
             fontWeight: FontWeight.bold,
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildSearch() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          Expanded(child: DropdownButton<School>(
+            value: _selectedSchool,
+            items: _schools.map((school) {
+              return DropdownMenuItem<School>(
+                value: school,
+                child: Text(school.naziv ?? "N/A"),
+              );
+            }).toList(),
+            onChanged: (School? newValue) {
+              setState(() {
+                _selectedSchool = newValue;
+                _selectedDepartment = null;
+                _selectedSubject = null;
+              });
+              _fetchSubjects();
+              _fetchDepartments();
+            },
+          ),),
+          Expanded(
+            child: DropdownButton<Department>(
+              hint: Text("Odjeljenje"),
+              value: _selectedDepartment,
+              onChanged: (Department? newValue) {
+                setState(() {
+                  _selectedDepartment = newValue;
+                });
+              },
+              items: _departments.map((Department department) {
+                return DropdownMenuItem<Department>(
+                  value: department,
+                  child: Text(department.nazivOdjeljenja ?? ""),
+                );
+              }).toList(),
+            ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            child: DropdownButton<Subject>(
+              hint: Text("Predmet"),
+              value: _selectedSubject,
+              onChanged: (Subject? newValue) {
+                setState(() {
+                  _selectedSubject = newValue;
+                });
+              },
+              items: _subjects.map((Subject subject) {
+                return DropdownMenuItem<Subject>(
+                  value: subject,
+                  child: Text(subject.naziv ?? ""),
+                );
+              }).toList(),
+            ),
+          ),
+          SizedBox(width: 10),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.blue,
+            ),
+            onPressed: () async {
+              // await _fetchAnnualPlanPrograms();
+            },
+            child: Text("Pretraga"),
+          ),
+        ],
       ),
     );
   }
