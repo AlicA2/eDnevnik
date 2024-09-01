da9ad57818c8f8ce8a761842267273dce9b12207
Amar Alić <amar.alic@edu.fit.ba>
2024-09-01 21:49:35 +0200

    ChartAdded

--- eDnevnik/UI/ednevnik_admin/lib/screens/report_screen.dart
+++ eDnevnik/UI/ednevnik_admin/lib/screens/report_screen.dart
@@ -1,10 +1,14 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:ednevnik_admin/models/department.dart';
+import 'package:ednevnik_admin/models/grade.dart';
 import 'package:ednevnik_admin/models/school.dart';
 import 'package:ednevnik_admin/models/subject.dart';
+import 'package:ednevnik_admin/models/user.dart';
 import 'package:ednevnik_admin/providers/department_provider.dart';
-import 'package:ednevnik_admin/providers/department_subject_provider.dart';
+import 'package:ednevnik_admin/providers/grade_provider.dart';
 import 'package:ednevnik_admin/providers/school_provider.dart';
 import 'package:ednevnik_admin/providers/subject_provider.dart';
+import 'package:ednevnik_admin/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:ednevnik_admin/widgets/master_screen.dart';
 import 'package:provider/provider.dart';
@@ -20,6 +24,8 @@ class _ReportDetailScreenState extends State<ReportDetailScreen> {
   late DepartmentProvider _departmentProvider;
   late SubjectProvider _subjectProvider;
   late SchoolProvider _schoolProvider;
+  late GradeProvider _gradeProvider;
+  late UserProvider _userProvider;
 
   Department? _selectedDepartment;
   Subject? _selectedSubject;
@@ -28,6 +34,8 @@ class _ReportDetailScreenState extends State<ReportDetailScreen> {
   List<Department> _departments = [];
   List<Subject> _subjects = [];
   List<School> _schools = [];
+  List<Grade> _grades = [];
+  Map<int, User> _users = {};
 
   @override
   void initState() {
@@ -35,8 +43,37 @@ class _ReportDetailScreenState extends State<ReportDetailScreen> {
     _departmentProvider = context.read<DepartmentProvider>();
     _subjectProvider = context.read<SubjectProvider>();
     _schoolProvider = context.read<SchoolProvider>();
+    _gradeProvider = context.read<GradeProvider>();
+    _userProvider = context.read<UserProvider>();
 
     _fetchSchools();
+  }
+
+  Future<void> _fetchGrades() async {
+    try {
+      var grades = await _gradeProvider.get(filter: {
+        'DepartmentID': _selectedDepartment?.odjeljenjeID,
+        'SubjectID': _selectedSubject?.predmetID,
+        'SchoolID': _selectedSchool?.skolaID,
+      });
+      setState(() {
+        _grades = grades.result;
+      });
+
+      await _fetchUsers();
+    } catch (e) {
+    }
+  }
+
+  Future<void> _fetchUsers() async {
+    for (Grade grade in _grades) {
+      if (grade.korisnikID != null && !_users.containsKey(grade.korisnikID!)) {
+        User user = await _userProvider.getById(grade.korisnikID!);
+        setState(() {
+          _users[grade.korisnikID!] = user;
+        });
+      }
+    }
   }
 
   Future<void> _fetchSchools() async {
@@ -90,6 +127,8 @@ class _ReportDetailScreenState extends State<ReportDetailScreen> {
                 _buildScreenName(),
                 SizedBox(height: 16.0),
                 _buildSearch(),
+                SizedBox(height: 20),
+                _buildChart(),
               ],
             ),
           ),
@@ -129,24 +168,26 @@ class _ReportDetailScreenState extends State<ReportDetailScreen> {
       padding: const EdgeInsets.all(20.0),
       child: Row(
         children: [
-          Expanded(child: DropdownButton<School>(
-            value: _selectedSchool,
-            items: _schools.map((school) {
-              return DropdownMenuItem<School>(
-                value: school,
-                child: Text(school.naziv ?? "N/A"),
-              );
-            }).toList(),
-            onChanged: (School? newValue) {
-              setState(() {
-                _selectedSchool = newValue;
-                _selectedDepartment = null;
-                _selectedSubject = null;
-              });
-              _fetchSubjects();
-              _fetchDepartments();
-            },
-          ),),
+          Expanded(
+            child: DropdownButton<School>(
+              value: _selectedSchool,
+              items: _schools.map((school) {
+                return DropdownMenuItem<School>(
+                  value: school,
+                  child: Text(school.naziv ?? "N/A"),
+                );
+              }).toList(),
+              onChanged: (School? newValue) {
+                setState(() {
+                  _selectedSchool = newValue;
+                  _selectedDepartment = null;
+                  _selectedSubject = null;
+                });
+                _fetchSubjects();
+                _fetchDepartments();
+              },
+            ),
+          ),
           Expanded(
             child: DropdownButton<Department>(
               hint: Text("Odjeljenje"),
@@ -189,12 +230,76 @@ class _ReportDetailScreenState extends State<ReportDetailScreen> {
               backgroundColor: Colors.blue,
             ),
             onPressed: () async {
-              // await _fetchAnnualPlanPrograms();
+              await _fetchGrades();
             },
             child: Text("Pretraga"),
           ),
         ],
       ),
     );
+  }
+
+  Widget _buildChart() {
+    if (_grades.isEmpty) {
+      return Center(child: Text("No grades available"));
+    }
+
+    return Container(
+      height: 300,
+      padding: EdgeInsets.all(16.0),
+      child: BarChart(
+        BarChartData(
+          alignment: BarChartAlignment.spaceAround,
+          barGroups: _buildBarGroups(),
+          borderData: FlBorderData(show: false),
+          titlesData: FlTitlesData(
+            bottomTitles: AxisTitles(
+              sideTitles: SideTitles(
+                showTitles: true,
+                getTitlesWidget: (value, meta) => _buildBottomTitles(value),
+              ),
+            ),
+            leftTitles: AxisTitles(
+              sideTitles: SideTitles(
+                showTitles: true,
+                getTitlesWidget: (value, meta) => Text(value.toInt().toString()),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<BarChartGroupData> _buildBarGroups() {
+    return _grades.asMap().entries.map((entry) {
+      int index = entry.key;
+      Grade grade = entry.value;
+      return BarChartGroupData(
+        x: index,
+        barRods: [
+          BarChartRodData(
+            toY: grade.vrijednostOcjene?.toDouble() ?? 0.0,
+            rodStackItems: [
+              BarChartRodStackItem(
+                0,
+                grade.vrijednostOcjene?.toDouble() ?? 0.0,
+                Colors.blue,
+              ),
+            ],
+            borderSide: BorderSide(
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      );
+    }).toList();
+  }
+
+  Widget _buildBottomTitles(double value) {
+    final index = value.toInt();
+    if (index < 0 || index >= _grades.length) return const Text('');
+    final user = _users[_grades[index].korisnikID];
+    return Text(user?.ime ?? 'N/A');
   }
 }
