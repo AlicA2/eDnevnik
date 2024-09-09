fca6528e5e17a0371d1f29dd1ba1156b887f4c48
Amar Alić <amar.alic@edu.fit.ba>
2024-09-09 18:38:32 +0200

    Update report_screen

--- eDnevnik/UI/ednevnik_admin/lib/screens/help_support_screen.dart
+++ eDnevnik/UI/ednevnik_admin/lib/screens/help_support_screen.dart
@@ -18,6 +18,35 @@ class HelpPage extends StatelessWidget {
       "Također ovdje će se moći pravdati časovi ukoliko to bude trebalo",
     ];
 
+    final List<String> godisnjiPlanProgramHelpItems = [
+      "Na ovom ekranu moći će se dodavati godišnji planovi i program",
+      "Bitno je napomenuti da ovi planovi i programi će biti vezani striktno za odjeljenje i predmeta za to odjeljenje",
+      "Broj časova je stavljen da može biti od 5 do 10 jer kada bi stavili veći broj trebalo bi sve te časove popuniti, kasnije ukoliko bude trebalo dodavati više časova možemo promjenuti u backendu i time demonstrirati Dependency Injection",
+      "Broj časova se automatski popunjava u kalendaru",
+      "Bitno je navesti da se mogu editovati godišnji planovi i programi, sva polja imaju svoju zasebnu validaciju",
+      "Ne mogu se dodavati godišnji planovi i programi koji su već u tabeli",
+      "Iz ovog ekrana možemo se prebaciti na Časovi ekran pomoću strelice prema desno"
+    ];
+
+    final List<String> casoviHelpItems = [
+      "Na ovom ekranu mi dodajemo časove za taj godišnji plan i program",
+      "Mora se unjeti tačan broj časova koji smo definirali u godišnjem planu i programu",
+      "U dodaj časovima svako polje ima svoju validaciju",
+      "Pretraga se vrši pomoću naziva časa"
+    ];
+
+    final List<String> izvjestajHelpItems = [
+      "Na ovome ekranu vrši se automatsko filtriranje na osnovu vrijednosti unutar comboboxova",
+      "Dugme printaj izvještaj daje pdf koji će se sačuvati na našem računaru lokalno",
+    ];
+
+    final List<String> odjeljenjaHelpItems = [
+      "Na ovome ekranu prikazana su odjeljenja za određenu školu",
+      "Dodavaju se predmeti i prikazivaju se učenici za određeno odjeljenje",
+      "U dodavanju odjeljenja sva polja imaju svoju validaciju",
+      "Moguće uređivanje odjeljenja"
+    ];
+
     return MasterScreenWidget(
       child: Container(
         color: const Color(0xFFF7F2FA),
@@ -28,20 +57,39 @@ class HelpPage extends StatelessWidget {
               color: Colors.white,
               borderRadius: BorderRadius.circular(20.0),
             ),
-            child: Column(
-              children: [
-                _buildScreenHeader(),
-                SizedBox(height: 16.0),
-                _buildMainHeader(),
-                SizedBox(height: 16.0),
-                _buildSectionHeader("Predmeti ekran"),
-                _buildHelpItemsList(predmetiHelpItems),
-                SizedBox(height: 16.0),
-                _buildSectionHeader("Druga sekcija"),
-                _buildHelpItemsList(porukeHelpItems),
-                SizedBox(height: 16.0),
-                _buildSectionHeader("Treća sekcija"),
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  _buildScreenHeader(),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 16.0),
+                        _buildMainHeader(),
+                        SizedBox(height: 16.0),
+                        _buildSectionHeader("Predmeti"),
+                        _buildHelpItemsList(predmetiHelpItems),
+                        SizedBox(height: 16.0),
+                        _buildSectionHeader("Poruke"),
+                        _buildHelpItemsList(porukeHelpItems),
+                        SizedBox(height: 16.0),
+                        _buildSectionHeader("Godišnji plan program"),
+                        _buildHelpItemsList(godisnjiPlanProgramHelpItems),
+                        SizedBox(height: 16.0),
+                        _buildSectionHeader("Časovi"),
+                        _buildHelpItemsList(casoviHelpItems),
+                        SizedBox(height: 16.0),
+                        _buildSectionHeader("Izvještaj"),
+                        _buildHelpItemsList(izvjestajHelpItems),
+                        SizedBox(height: 16.0),
+                        _buildSectionHeader("Odjeljenja"),
+                        _buildHelpItemsList(odjeljenjaHelpItems),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -105,6 +153,7 @@ class HelpPage extends StatelessWidget {
   Widget _buildHelpItemsList(List<String> items) {
     return ListView.builder(
       shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
       padding: const EdgeInsets.all(16.0),
       itemCount: items.length,
       itemBuilder: (context, index) {
